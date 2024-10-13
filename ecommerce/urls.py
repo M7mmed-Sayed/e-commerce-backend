80623572c1391c3810b087d34584b2fe1791fd5d
m7mmed-sayed <mohamedsayed1167@gmail.com>
2024-10-13 10:54:26 +0300

    add cart to project settings

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path("products/", include("products.urls")),
+    path("cart/", include("cart.urls")),
 
 
 ]
